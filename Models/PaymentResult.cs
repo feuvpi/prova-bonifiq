1deef0c09610212931bf588610325f5b3e525ae5
Fred Pinheiro <fredvpg@gmail.com>
2023-04-11 14:30:19 -0300

    Adicionando o modelo PaymentResult

---
+++ Models/PaymentResult.cs
@@ -0,0 +1,21 @@
+﻿namespace ProvaPub.Models
+{
+    public class PaymentResult
+    {
+        #region [ ATRIBUTES ]
+        public string TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public PaymentStatus Status { get; set; }
+        #endregion
+
+        public enum PaymentStatus
+        {
+            Success,
+            Failure,
+            Pending
+        }
+
+
+    }
+}
